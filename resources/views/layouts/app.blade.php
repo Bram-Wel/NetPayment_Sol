0222da131e9ec4beaa48efb386c8170b3ea29cde
fabianmuema <muemafabian@gmail.com>
2021-01-16 14:34:20 +0300

    command

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,11 @@
 <body class="font-sans antialiased">
 <div class="min-h-screen bg-gray-100">
     @livewire('navigation-dropdown')
+    <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            {{ $header }}
+        </div>
+    </header>
     <main>
         {{ $slot ?? null }}
     </main>

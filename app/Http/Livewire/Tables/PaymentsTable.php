658a18197a931932d5cd430f2484fe49b2e7f148
fabianmuema <muemafabian@gmail.com>
2021-01-17 11:24:52 +0300

    payments table

---
+++ app/Http/Livewire/Tables/PaymentsTable.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Livewire\Tables;
+
+use App\Models\Payment;
+use Mediconesystems\LivewireDatatables\BooleanColumn;
+use Mediconesystems\LivewireDatatables\Column;
+use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
+use Mediconesystems\LivewireDatatables\NumberColumn;
+
+class PaymentsTable extends LivewireDatatable
+{
+    public $model = Payment::class;
+
+    public function builder()
+    {
+
+    }
+
+    public function columns()
+    {
+        return [
+            NumberColumn::name('id'),
+            Column::name('phone'),
+            Column::name('receipt_number')->label('Receipt'),
+            Column::name('amount'),
+            Column::name('type'),
+            Column::callback(['created_at'], function ($date) {
+                return date('d, M Y h:i:s A', strtotime($date));
+            }),
+            BooleanColumn::name('checked')
+        ];
+    }
+}
--- resources/views/payments.blade.php
+++ resources/views/payments.blade.php
@@ -2,7 +2,8 @@
     <x-slot name="header">
         <div class="flex flex-row">
             <h1 class="text-xl font-bold text-blue-500 border-b border-blue-500 border-gray-500 w-24">{{ __('Payments') }}</h1>
-            <a href="{{ route('record-payment') }}" class="absolute right-20 text-white font-bold bg-green-400 px-3 py-1 shadow-lg rounded-2xl">Record
+            <a href="{{ route('record-payment') }}"
+               class="absolute right-20 text-white font-bold bg-green-400 px-3 py-1 shadow-lg rounded-2xl">Record
                 payment</a>
         </div>
     </x-slot>
@@ -11,60 +12,7 @@
             <div class="flex flex-col md:flex-row justify-center">
                 <div class="p-3">
                     <h1 class="pt-2 pb-2 text-2xl">Payments</h1>
-                    <table class="table table-fixed min-w-full leading-normal">
-                        <thead>
-                        <tr>
-                            <th class="px-10 py-3 border-b border-t border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                #
-                            </th>
-                            <th class="px-10 py-3 border-b border-t border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                Username
-                            </th>
-                            <th class="px-10 py-3 border-b border-t border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                Phone
-                            </th>
-                            <th class="px-10 py-3 border-b border-t border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                Receipt Number
-                            </th>
-                            <th class="px-10 py-3 border-b border-t border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                amount
-                            </th>
-                            <th class="px-10 py-3 border-b border-t border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                Time
-                            </th>
-                            <th class="px-10 py-3 border-b border-t border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($payments as $index=>$payment)
-                            <tr>
-                                <td class="px-10 py-2 border-b border-gray-200 bg-white text-sm">
-                                    {{ $index + 1 }}
-                                </td>
-                                <td class="px-10 py-2 border-b border-gray-200 bg-white text-sm">
-                                    <?php
-                                    $username = \Illuminate\Support\Facades\DB::table('users')->where('phone', $payment->phone)->value('username');
-                                    ?>
-                                    {{ $username }}
-                                </td>
-                                <td class="px-10 py-2 border-b border-gray-200 bg-white text-sm">
-                                    {{ $payment->phone}}
-                                </td>
-                                <td class="px-10 py-2 border-b border-gray-200 bg-white text-sm">
-                                    {{ $payment->receipt_number }}
-                                </td>
-                                <td class="px-10 py-2 border-b border-gray-200 bg-white text-sm">
-                                    {{ $payment->amount }}
-                                </td>
-                                <td class="px-10 py-2 border-b border-gray-200 bg-white text-sm">
-                                    {{ date('d/M/Y h:i A', strtotime($payment->created_at)) }}
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                    @livewire('tables.payments-table')
                 </div>
             </div>
             <div class="p-5">

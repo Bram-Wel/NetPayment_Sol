175c60b843d9294c16fee756a739c5fd9920cd3d
fabianmuema <muemafabian@gmail.com>
2021-01-17 10:17:56 +0300

    payments fraph

--- resources/views/livewire/active-users.blade.php
+++ resources/views/livewire/active-users.blade.php
@@ -1,4 +1,4 @@
-<div wire:poll.10000ms="activeUsers" class="flex flex-row content-between bg-white p-5 rounded shadow-xl"
+<div class="flex flex-row content-between bg-white p-5 rounded shadow-xl"
      style="width: 18rem">
         <span class="icon bg-green-100 rounded-full w-12 flex align-middle content-center" style="align-items: center"><ion-icon
                 style="font-size: 25px" class="flex content-center align-middle mx-auto" name="wifi-outline"></ion-icon></span>

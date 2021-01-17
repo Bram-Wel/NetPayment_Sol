15c04f53ea8c764a4a2debd96a00eb7a1b4fb0d8
fabianmuema <muemafabian@gmail.com>
2021-01-17 20:46:32 +0300

    adjust width

--- resources/views/payments/payment-clicks.blade.php
+++ resources/views/payments/payment-clicks.blade.php
@@ -3,7 +3,7 @@
         <div class="flex flex-row">
             <h1 class="text-xl font-bold text-blue-500 border-b border-blue-500 border-gray-500 w-48">{{ __('Payment Clicks') }}</h1>
             <a href="{{ \Illuminate\Support\Facades\URL::previous() }}"
-               class="absolute right-20 text-white font-bold bg-green-400 px-3 py-1 shadow-lg rounded-2xl">bACK</a>
+               class="absolute right-20 text-white font-bold bg-green-400 px-3 py-1 shadow-lg rounded-2xl">Go back</a>
         </div>
     </x-slot>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-10">

36e00dbb15ac8c0960864b5c41579816f13114ab
fabianmuema <muemafabian@gmail.com>
2021-01-17 16:10:52 +0300

    authentication card

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 mx-10">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 mx-10">
     <div>
         {{ $logo }}
     </div>

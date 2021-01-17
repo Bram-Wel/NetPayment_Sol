9d614b74846fed36d5d5fccc77467d918bd3ad5c
fabianmuema <muemafabian@gmail.com>
2021-01-17 20:43:23 +0300

    payment clicks

--- resources/views/payments/payment-clicks.blade.php
+++ resources/views/payments/payment-clicks.blade.php
@@ -1,10 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex flex-row">
-            <h1 class="text-xl font-bold text-blue-500 border-b border-blue-500 border-gray-500 w-24">{{ __('Payments') }}</h1>
-            <a href="{{ route('record-payment') }}"
-               class="absolute right-20 text-white font-bold bg-green-400 px-3 py-1 shadow-lg rounded-2xl">Record
-                payment</a>
+            <h1 class="text-xl font-bold text-blue-500 border-b border-blue-500 border-gray-500 w-24">{{ __('Payment Clicks') }}</h1>
+            <a href="{{ \Illuminate\Support\Facades\URL::previous() }}"
+               class="absolute right-20 text-white font-bold bg-green-400 px-3 py-1 shadow-lg rounded-2xl">bACK</a>
         </div>
     </x-slot>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-10">

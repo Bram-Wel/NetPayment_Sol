1feb851aa62bee2ce16a924172a260eb49cc4e43
fabianmuema <muemafabian@gmail.com>
2021-01-17 20:28:07 +0300

    total amount today

---
+++ app/Http/Livewire/Tables/PaymentClicksTable.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Livewire\Tables;
+
+use App\Models\Ip;
+use App\Models\User;
+use Mediconesystems\LivewireDatatables\Column;
+use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
+use Mediconesystems\LivewireDatatables\NumberColumn;
+
+class PaymentClicksTable extends LivewireDatatable
+{
+    public $model = Ip::class;
+
+    public function columns()
+    {
+        return [
+            NumberColumn::name('id'),
+            Column::callback(['phone'], function ($phone) {
+                return User::where('phone', 0 . ltrim($phone, '254'))->value('username');
+            })->label('Username'),
+            Column::callback(['phone', 'address'], function ($phone) {
+                return 0 . ltrim($phone, '254');
+            })->label('Phone'),
+            Column::name('address'),
+            Column::callback(['created_at'], function ($date) {
+                return date('d, M Y h:i:s A', strtotime($date));
+            })
+        ];
+    }
+}
---
+++ resources/views/payments/payment-clicks.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex flex-row">
+            <h1 class="text-xl font-bold text-blue-500 border-b border-blue-500 border-gray-500 w-24">{{ __('Payments') }}</h1>
+            <a href="{{ route('record-payment') }}"
+               class="absolute right-20 text-white font-bold bg-green-400 px-3 py-1 shadow-lg rounded-2xl">Record
+                payment</a>
+        </div>
+    </x-slot>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-10">
+        <div class="flex flex-col md:flex-row justify-center">
+            <div class="p-3">
+                @livewire('tables.payments-clicks-table')
+            </div>
+        </div>
+    </div>
+</x-app-layout>

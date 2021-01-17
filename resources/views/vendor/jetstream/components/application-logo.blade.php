bc8f539832d25644961bd6c6cbce9d8978bfa044
fabianmuema <muemafabian@gmail.com>
2021-01-17 15:37:58 +0300

    add name

--- resources/views/vendor/jetstream/components/application-logo.blade.php
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('/images/logo.png') }}" alt="logo" style="width: 200px">
+<img src="{{ asset('/images/logo.png') }}" alt="logo" style="width: 150px">
--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/" class="text-4xl">
-    <img src="{{ asset('/images/logo.png') }}" alt="logo" style="width: 160px">
+    <img src="{{ asset('/images/logo.png') }}" alt="logo" style="width: 140px">
 </a>

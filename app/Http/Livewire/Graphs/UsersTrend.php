0851eab2061e9538730b8b9318381a00586904f0
fabianmuema <muemafabian@gmail.com>
2021-01-18 18:02:50 +0300

    users trend

--- app/Http/Livewire/Graphs/UsersTrend.php
+++ app/Http/Livewire/Graphs/UsersTrend.php
@@ -14,14 +14,14 @@ class UsersTrend extends Component
 
     public function mount()
     {
-        $this->thisYear = DB::table('users')->whereYear('created_at', '=', now()->year)->count();
-        $this->thisMonth = DB::table('users')->whereMonth('created_at', '=', now()->month)->count();
-        $this->thisWeek = DB::table('users')->whereBetween('created_at', [Carbon::now()->startOfWeek('1'), Carbon::now()->endOfWeek('7')])->count();
-        $this->lastYear = DB::table('users')->whereYear('created_at', '=', now()->year - 1)->count();
-        $this->secondLastYear = DB::table('users')->whereYear('created_at', '=', now()->year - 2)->count();
-        $this->thirdLastYear = DB::table('users')->whereYear('created_at', '=', now()->year - 3)->count();
-        $this->fourthLastYear = DB::table('users')->whereYear('created_at', '=', now()->year - 4)->count();
-        $this->fifthLastYear = DB::table('users')->whereYear('created_at', '=', now()->year - 5)->count();
+        $this->thisYear = DB::table('users')->whereYear('created_at', '=', now()->year)->count('id');
+        $this->thisMonth = DB::table('users')->whereMonth('created_at', '=', now()->month)->count('id');
+        $this->thisWeek = DB::table('users')->whereBetween('created_at', [Carbon::now()->startOfWeek('1'), Carbon::now()->endOfWeek('7')])->count('id');
+        $this->lastYear = DB::table('users')->whereYear('created_at', '=', now()->year - 1)->count('id');
+        $this->secondLastYear = DB::table('users')->whereYear('created_at', '=', now()->year - 2)->count('id');
+        $this->thirdLastYear = DB::table('users')->whereYear('created_at', '=', now()->year - 3)->count('id');
+        $this->fourthLastYear = DB::table('users')->whereYear('created_at', '=', now()->year - 4)->count('id');
+        $this->fifthLastYear = DB::table('users')->whereYear('created_at', '=', now()->year - 5)->count('id');
 
         $this->one = now()->year;
         $this->two = now()->year - 1;

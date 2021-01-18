f904794219cc03e9943425d2d1075b772e1850bb
fabianmuema <muemafabian@gmail.com>
2021-01-18 18:35:26 +0300

    payment clicks

--- resources/views/payments.blade.php
+++ resources/views/payments.blade.php
@@ -3,7 +3,7 @@
         <div class="flex flex-row">
             <h1 class="text-xl font-bold text-blue-500 mr-10">{{ __('Payments') }}</h1>
             <a href="{{ route('payment.clicks') }}"
-               class="text-xl font-bold text-blue-500 ">{{ __('Payment Clicks') }}</a>
+               class="text-xl font-bold text-blue-500 ">{{ __('Payment clicks') }}</a>
 
             <a href="{{ route('record-payment') }}"
                class="absolute right-20 text-white font-bold bg-green-400 px-3 py-1 shadow-lg rounded-2xl">Record

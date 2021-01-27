d37f372e700c2a179f7184325ac6c5c610fb1bf7
fabianmuema <muemafabian@gmail.com>
2021-01-27 18:33:36 +0300

    latest movies

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,11 +1,12 @@
 @include('movies.layouts.default')
 <div class="py-5">
-    <div class="flex flex-row flex-wrap justify-center">
+    <h1 class="text-gray-600 text-xl pl-6">Latest movies</h1>
+    <div class="flex flex-row flex-wrap justify-start">
         @foreach($movies as $movie)
             <?php
             $url = \Illuminate\Support\Facades\Storage::disk('movies')->url($movie->name . '/poster.jpg');
             ?>
-            <a href="{{ route('player', ['movie' => $movie->id]) }}" class="ml-6 mb-6 mt-6">
+            <a href="{{ route('player', ['movie' => $movie->id]) }}" class="ml-6 mb-6 mt-2">
                 <div
                     style="background: url('{{ $url }}'); background-size: cover; width: 180px; height: 250px; background-position: center; background-repeat: no-repeat"
                     class="rounded-lg shadow-lg">

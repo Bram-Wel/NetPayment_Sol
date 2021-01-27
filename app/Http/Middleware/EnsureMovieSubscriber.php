61923bf55a907d5e7a915945abff000d0ec17544
fabianmuema <muemafabian@gmail.com>
2021-01-27 18:19:33 +0300

    unsubscibe

--- app/Http/Middleware/EnsureMovieSubscriber.php
+++ app/Http/Middleware/EnsureMovieSubscriber.php
@@ -6,20 +6,23 @@ use App\Models\MovieSubscription;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class EnsureMovieSubscriber
 {
     /**
      * Handle an incoming request.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
         $name = Auth::user()->name;
-        $count = MovieSubscription::where('name', $name)->count('id');
+        DB::table('movie_subscriptions')
+            ->where('name', $name)
+            ->count('id');
         if ($count == 0) {
             session()->flash('message', 'Kindly subscribe to watch movies!');
             return redirect()->to(route('movie-packages'));

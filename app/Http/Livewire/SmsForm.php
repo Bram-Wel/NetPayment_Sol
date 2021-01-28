f67c7fb61d9c84c6cd43a1afe3a7875e0fde3582
fabianmuema <muemafabian@gmail.com>
2021-01-28 14:33:17 +0300

    latest movies

--- app/Http/Livewire/SmsForm.php
+++ app/Http/Livewire/SmsForm.php
@@ -13,31 +13,56 @@ class SmsForm extends Component
     public $user, $message;
 
     protected $rules = [
-        'user' => 'required',
         'message' => 'required|min:20'
     ];
 
     public function sendSms()
     {
-        $phone = User::where('username', $this->user)->value('phone');
-        $username = "thetechglitch"; // use 'sandbox' for development in the test environment
-        $apiKey = '0355a696e0f0dca94141e9f88dddd738cdcfc98725445473b0e182b7a15fc526'; // use your sandbox app API key for development in the test environment
-        $AT = new AfricasTalking($username, $apiKey);
+        if ($this->user == '') {
+            $users = User::get();
+            foreach ($users as $user) {
+                $phone = $user->phone;
+                $username = "thetechglitch"; // use 'sandbox' for development in the test environment
+                $apiKey = '0355a696e0f0dca94141e9f88dddd738cdcfc98725445473b0e182b7a15fc526'; // use your sandbox app API key for development in the test environment
+                $AT = new AfricasTalking($username, $apiKey);
 
-        $sms = $AT->sms();
+                $sms = $AT->sms();
 
-        $sms->send([
-            'to' => '+254' . ltrim($phone, '0'),
-            'message' => $this->message
-        ]);
+                $sms->send([
+                    'to' => '+254' . ltrim($phone, '0'),
+                    'message' => $this->message
+                ]);
 
-        $message = new Message();
-        $message->username = $this->user;
-        $message->phone = $phone;
-        $message->email = User::where('phone', $phone)->value('email');
-        $message->message = $this->message;
-        $message->type = 'sms';
-        $message->save();
+                $message = new Message();
+                $message->username = $this->user;
+                $message->phone = $phone;
+                $message->email = User::where('phone', $phone)->value('email');
+                $message->message = $this->message;
+                $message->type = 'sms';
+                $message->save();
+            }
+        } else {
+            $phone = User::where('username', $this->user)->value('phone');
+            $username = "thetechglitch"; // use 'sandbox' for development in the test environment
+            $apiKey = '0355a696e0f0dca94141e9f88dddd738cdcfc98725445473b0e182b7a15fc526'; // use your sandbox app API key for development in the test environment
+            $AT = new AfricasTalking($username, $apiKey);
+
+            $sms = $AT->sms();
+
+            $sms->send([
+                'to' => '+254' . ltrim($phone, '0'),
+                'message' => $this->message
+            ]);
+
+            $message = new Message();
+            $message->username = $this->user;
+            $message->phone = $phone;
+            $message->email = User::where('phone', $phone)->value('email');
+            $message->message = $this->message;
+            $message->type = 'sms';
+            $message->save();
+        }
+
 
         $this->reset();
     }

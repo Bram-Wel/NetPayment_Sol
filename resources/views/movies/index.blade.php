564b892ed1c92aeae9ff8b345afe10ba03ea9fc1
fabianmuema <muemafabian@gmail.com>
2021-01-28 14:39:09 +0300

    latest movies

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -14,8 +14,9 @@
     {{--        @endforeach--}}
     {{--    </div>--}}
     <div>
-        <h1 class="text-gray-600 text-xl pl-6">Latest movies</h1>
-        <div class="flex flex-row flex-wrap justify-start">
+        <h1 class="text-gray-600 text-xl pl-6 text-center md:text-left
+">Latest movies</h1>
+        <div class="flex flex-row flex-wrap justify-center md:justify-start">
             @foreach($movies as $movie)
                 @php
                     $url = \Illuminate\Support\Facades\Storage::disk('movies')->url($movie->name . '/poster.jpg');

762138f8520bdbb842ef98c6bc3a825a22728079
fabianmuema <muemafabian@gmail.com>
2021-01-28 14:36:44 +0300

    latest movies

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -16,9 +16,9 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Internet Packages') }}
                     </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('movie-packages') }}" :active="request()->routeIs('movie-packages')">
-                        {{ __('Movie Packages') }}
-                    </x-jet-nav-link>
+                    {{--                    <x-jet-nav-link href="{{ route('movie-packages') }}" :active="request()->routeIs('movie-packages')">--}}
+                    {{--                        {{ __('Movie Packages') }}--}}
+                    {{--                    </x-jet-nav-link>--}}
                     <x-jet-nav-link href="{{ route('movies') }}" :active="request()->routeIs('movies')">
                         {{ __('Movies') }}
                     </x-jet-nav-link>
@@ -161,6 +161,10 @@
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Packages') }}
             </x-jet-responsive-nav-link>
+            <x-jet-responsive-nav-link href="{{ route('movies') }}" :active="request()->routeIs('movies')">
+                {{ __('Movies') }}
+            </x-jet-responsive-nav-link>
+
         </div>
 
         <!-- Responsive Settings Options -->

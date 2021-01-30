9ea32d49968d04c32c253f8dd2e9135e4176e221
fabianmuema <muemafabian@gmail.com>
2021-01-30 10:15:24 +0300

    url encode

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -27,7 +27,7 @@
                 <a href="{{ route('player', ['movie' => $movie->id]) }}" class="md:ml-6 mb-6 mt-2">
                     @if($index==0 || $index ==1)
                         <div
-                            style="background: url('{{ $url }}'); background-size: cover; background-position: center; background-repeat: no-repeat; width: 220px; height: 350px"
+                            style="background: url('{{ urlencode($url) }}'); background-size: cover; background-position: center; background-repeat: no-repeat; width: 220px; height: 350px"
                             class="shadow-xl rounded-xl">
                         </div>
                     @elseif($index==2)
@@ -56,7 +56,8 @@
                 @endphp
                 <a href="{{ route('player', ['movie' => $movie->id]) }}" class="md:ml-6 mb-6 mt-2">
                     <div
-                        style="background: url('{{ $url }}'); background-size: cover; background-position: center; background-repeat: no-repeat"
+                        style="
+                            background: url('{{ urlencode($url) }}'); background-size: cover; background-position: center; background-repeat: no-repeat"
                         class="rounded-lg shadow-xl poster">
                     </div>
                 </a>

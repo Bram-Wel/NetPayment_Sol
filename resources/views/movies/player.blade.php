052b65d867f8ecb9e982009bff9519216ba53118
fabianmuema <muemafabian@gmail.com>
2021-01-31 13:35:35 +0300

    lazy load

--- resources/views/movies/player.blade.php
+++ resources/views/movies/player.blade.php
@@ -10,6 +10,12 @@
 
     video {
         object-fit: contain;
+        left: 50%;
+        min-height: 100%;
+        min-width: 100%;
+        position: absolute;
+        top: 50%;
+        transform: translate(-50%, -50%);
     }
 </style>
 

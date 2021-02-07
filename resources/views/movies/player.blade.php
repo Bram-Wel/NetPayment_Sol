2199d207575f7d41e12630060b72b51c4097173f
fabianmuema <muemafabian@gmail.com>
2021-02-07 16:45:29 +0300

    detect volume change

--- resources/views/movies/player.blade.php
+++ resources/views/movies/player.blade.php
@@ -8,6 +8,10 @@
     <title>{{ $movie }}</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="{{ mix('js/app.js') }}">
+    <script
+        src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+        crossorigin="anonymous"></script>
 
     <style>
         body {

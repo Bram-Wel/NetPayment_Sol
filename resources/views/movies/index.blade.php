239a6b603f7b1c4afea3e09aa9176574191aefed
fabianmuema <muemafabian@gmail.com>
2021-02-07 15:40:55 +0300

    specify volume

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -14,7 +14,12 @@
     }
 
     video {
-        filter: brightness(50%);
+        position: absolute;
+        top: 0;
+        left: 0;
+        display: block;
+        width: 100%;
+        height: 100%;
         object-fit: cover;
     }
 

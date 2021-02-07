8db4f331a4ce8e0d788cce5298634aeb97d523a4
fabianmuema <muemafabian@gmail.com>
2021-02-07 10:48:47 +0300

    get all directories

--- app/Console/Commands/DownloadTrailers.php
+++ app/Console/Commands/DownloadTrailers.php
@@ -40,29 +40,23 @@ class DownloadTrailers extends Command
      */
     public function handle()
     {
-        $movies = Movie::orderBy('id')->get();
-        foreach ($movies as $movie) {
-            $trailerExists = Trailers::where('movie', $movie->name)->count('id', 'desc');
-            if ($trailerExists == 0) {
-                $files = Storage::disk($movie->disk)->directories();
-                foreach ($files as $file) {
-                    $file_parts = pathinfo($file);
-                    $directory = $file_parts['dirname'];
-                    $file = escapeshellarg($file);
-                    if ($movie->disk == 'movies2') {
-                        $directory = escapeshellarg("/run/media/thetechglitch/MOVIES/$directory");
-                    } else {
-                        $directory = "/srv/http/thetechglitch_internet/storage/app/public/movies/$directory";
-                    }
-                    print_r($directory);
-                    chdir($directory);
-                    shell_exec("youtube-dl -f best $movie->trailer --output 'trailer.%(ext)s'");
-                }
-                $trailer = new Trailers();
-                $trailer->movie = $movie->name;
-                $trailer->downloaded = true;
-                $trailer->save();
+        $files = Storage::disk($movie->disk)->directories();
+        foreach ($files as $file) {
+            $file_parts = pathinfo($file);
+            $directory = $file_parts['dirname'];
+            $file = escapeshellarg($file);
+            if ($movie->disk == 'movies2') {
+                $directory = escapeshellarg("/run/media/thetechglitch/MOVIES/$directory");
+            } else {
+                $directory = "/srv/http/thetechglitch_internet/storage/app/public/movies/$directory";
             }
+            print_r($directory);
+            chdir($directory);
+            shell_exec("youtube-dl -f best $movie->trailer --output 'trailer.%(ext)s'");
         }
+        $trailer = new Trailers();
+        $trailer->movie = $movie->name;
+        $trailer->downloaded = true;
+
     }
 }

9f50a2a85f579fa8905c7c5faa308f4405aedf12
fabianmuema <muemafabian@gmail.com>
2021-02-07 13:59:09 +0300

    download trailers

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('add:movies')
             ->withoutOverlapping(10)
-            ->everyTwoHours();
+            ->everyThreeMinutes();
 
         $schedule->command('movie:payment')
             ->withoutOverlapping(10)

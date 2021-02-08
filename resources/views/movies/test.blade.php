b6a0aab620bdc283b2d5cb856a502ce53fba3010
fabianmuema <muemafabian@gmail.com>
2021-02-08 14:12:02 +0300

    remove slick

--- resources/views/movies/test.blade.php
+++ resources/views/movies/test.blade.php
@@ -109,31 +109,19 @@
     <div class="wrapper">
         <section id="section1">
             <a href="#section3" class="arrow__btn">‹</a>
-            <div class="item">
-                <img
-                    src="https://occ-0-1567-1123.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABRvngexxF8H1-OzRWFSj6ddD-aB93tTBP9kMNz3cIVfuIfLEP1E_0saiNAwOtrM6xSOXvoiSCMsihWSkW0dq808-R7_lBnr6WHbjkKBX6I3sD0uCcS8kSPbRjEDdG8CeeVXEAEV6spQ.webp"
-                    alt="Describe Image">
-            </div>
-            <div class="item">
-                <img
-                    src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABZEK-7pZ1H5FD4cTyUb9qB_KeyJGz5p-kfPhCFv4GU_3mbdm8Xfsy4IBchlG9PFNdGff8cBNPaeMra72VFnot41nt0y3e8RLgaVwwh3UvyM2H2_MkmadWbQUeGuf811K7-cxJJh7gA.jpg"
-                    alt="Describe Image">
-            </div>
-            <div class="item">
-                <img
-                    src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABQCoK53qihwVPLRxPEDX98nyYpGbxgi5cc0ZOM4iHQu7KQvtgNyaNM5PsgI0vy5g3rLPZdjGCFr1EggrCPXpL77p2H08jV0tNEmIfs_e8KUfvBJ6Ay5nM4UM1dl-58xA6t1swmautOM.webp"
-                    alt="Describe Image">
-            </div>
-            <div class="item">
-                <img
-                    src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABdYtAqj8CyaJTWq5taD8Ro_UgwH3nne9QpFGVps-2J3IG-leqrfqXFii4jzZn48nPYTkrlwKQEV0R7_cEKlYBPRzdKqNODc-Oz26IL3LlLgFboXibIWXwxzeYxzuqn0I9TpARjeByw.jpg"
-                    alt="Describe Image">
-            </div>
-            <div class="item">
-                <img
-                    src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABbcCX42tsqGbBvO2y9CQv5-7QvYbCfoHtXsuc6NPCtZaKa4l4fBX3XWvUwG9F2A3CTsNpHVmulxBbdXKwK8Q6xGjejd9FoadGkZ7CnGrSl601TOQjzSHJ23NuIPC8j0QMGORL4uRIA.jpg"
-                    alt="Describe Image">
-            </div>
+            @php
+                $latest = \App\Models\Movie::orderBy('created_at', 'desc')->limit(7)->get();
+            @endphp
+            @foreach($latest as $movie)
+                @php
+                    $url = \Illuminate\Support\Facades\Storage::disk($movie->disk)->url($movie->name . '/poster.jpg');
+                @endphp
+                <div class="item">
+                    <img
+                        src="{{ $url }}"
+                        alt="Describe Image">
+                </div>
+            @endforeach
             <a href="#section2" class="arrow__btn">›</a>
         </section>
         <section id="section2">

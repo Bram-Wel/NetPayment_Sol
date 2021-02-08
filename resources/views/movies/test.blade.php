47d7dee6c2c4f200945b35125fb7d3e255e4bf44
fabianmuema <muemafabian@gmail.com>
2021-02-08 14:34:39 +0300

    remove slick

--- resources/views/movies/test.blade.php
+++ resources/views/movies/test.blade.php
@@ -106,7 +106,7 @@
         </div>
     @endforeach
 
-    <div class="wrapper pt-10/12">
+    <div class="wrapper pt-8/12">
         <section id="section1">
             <a href="#section3" class="arrow__btn">‹</a>
             @php

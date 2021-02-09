d880302c1f6abdf3dd0a5313483e691753a9464c
fabianmuema <muemafabian@gmail.com>
2021-02-09 09:35:53 +0300

    remove most watched

--- resources/views/movies/info.blade.php
+++ resources/views/movies/info.blade.php
@@ -14,12 +14,8 @@
     style="width: 100%; height: 100vh; background-size: cover; background-position: center"
     class="overflow-none main">
     <div class="absolute top-40 left-20">
-        @php
-            $file = file_exists($url . "/logo.jpg");
-
-        @endphp
-        @if(strlen($file) > 0)
-            <img src="{!! $url !!}/logo.jpg" alt="">
+        @if(file_exists($url . "/logo.jpg"));
+        <img src="{!! $url !!}/logo.jpg" alt="">
         @else
             <h1 class="text-white font-bold text-5xl">{{ $movie->name }}</h1>
         @endif

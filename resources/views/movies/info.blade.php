5b3d7e210abb33b15d02f92095660d68dabe7925
fabianmuema <muemafabian@gmail.com>
2021-02-09 09:00:06 +0300

    remove most watched

--- resources/views/movies/info.blade.php
+++ resources/views/movies/info.blade.php
@@ -2,8 +2,13 @@
 @php
     $url = \Illuminate\Support\Facades\Storage::disk($movie->disk)->url($movie->name);
 @endphp
+<style>
+    .main {
+        background: url("{!! $url !!}/fanart.jpg");
+    }
+</style>
 <div
-    style="background: url('{!! $url !!}/fanart.jpg'); width: 100%; height: 100vh; background-size: cover; background-position: center"
-    class="overflow-none">
+    style="width: 100%; height: 100vh; background-size: cover; background-position: center"
+    class="overflow-none main">
     {{  $movie->name }}
 </div>

ea263fcb8020fbf6a281cb809c7f5dcec71a3a7a
fabianmuema <muemafabian@gmail.com>
2021-02-09 08:35:30 +0300

    remove most watched

--- resources/views/movies/player.blade.php
+++ resources/views/movies/player.blade.php
@@ -115,7 +115,7 @@
             hls.attachMedia(video);
             // MEDIA_ATTACHED event is fired by hls object once MediaSource is ready
             hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-                hls.loadSource("{{ $url }}");
+                hls.loadSource("{{ htmlspecialchars($url) }}");
                 hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
                 });
             });

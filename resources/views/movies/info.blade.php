541317c629d73038934b135769860ef3bfc88fc7
fabianmuema <muemafabian@gmail.com>
2021-02-09 15:48:53 +0300

    movie details

--- resources/views/movies/info.blade.php
+++ resources/views/movies/info.blade.php
@@ -52,6 +52,10 @@
                class="mr-4 bg-white rounded-xl shadow-xl hover:shadow-2xl font-bold p-2 px-8 transition duration-200 hover:opacity-9 flex">
                 <ion-icon name="play-outline" class="pr-2 text-xl flex whitespace-no-wrap flex-col"></ion-icon>
                 Play</a>
+            <a href="{{ route('player', ['movie' => $movie->id]) }}"
+               class="mr-4 bg-white rounded-xl shadow-xl hover:shadow-2xl font-bold p-2 px-8 transition duration-200 hover:opacity-9 flex">
+                <ion-icon name="play-outline" class="pr-2 text-xl flex whitespace-no-wrap flex-col"></ion-icon>
+                Play trailer</a>
         </div>
     </div>
 </div>

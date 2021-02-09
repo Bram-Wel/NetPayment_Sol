4f858243656d3624deeceab1f7b250d22b44ca39
fabianmuema <muemafabian@gmail.com>
2021-02-09 15:06:12 +0300

    remove most watched

--- resources/views/movies/test.blade.php
+++ resources/views/movies/test.blade.php
@@ -86,7 +86,7 @@
     @endforeach
 
     <div class="pt-1/3">
-        <h1 class="font-bold text-xl pl-15 text-center md:text-left mt-8 relative z-20 text-white">Latest releases</h1>
+        <h1 class="font-bold text-xl pl-15 text-center md:text-left mt-12 relative z-20 text-white">Latest releases</h1>
         <div class="pl-8 grab">
             @foreach($movies as $movie)
                 @php

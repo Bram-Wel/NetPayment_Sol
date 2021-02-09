baff9552d6752f5579b4f04ec76ba08a1d2e0ce9
fabianmuema <muemafabian@gmail.com>
2021-02-09 09:23:07 +0300

    remove most watched

--- resources/views/movies/info.blade.php
+++ resources/views/movies/info.blade.php
@@ -14,6 +14,10 @@
     style="width: 100%; height: 100vh; background-size: cover; background-position: center"
     class="overflow-none main">
     <div class="absolute top-40 left-20">
-        <h1 class="text-white font-bold text-5xl">{{ $movie->name }}</h1>
+        @if(file_get_contents($url . "/logo.jpg"))
+            <img src="{!! $url !!}/logo.jpg" alt="">
+        @else
+            <h1 class="text-white font-bold text-5xl">{{ $movie->name }}</h1>
+        @endif
     </div>
 </div>

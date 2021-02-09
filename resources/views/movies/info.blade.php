a5be2e2a26c08597ffff824ec4bae71b94aa5e0d
fabianmuema <muemafabian@gmail.com>
2021-02-09 09:39:33 +0300

    remove most watched

--- resources/views/movies/info.blade.php
+++ resources/views/movies/info.blade.php
@@ -14,7 +14,23 @@
     style="width: 100%; height: 100vh; background-size: cover; background-position: center"
     class="overflow-none main">
     <div class="absolute top-40 left-20">
-        @if(file_exists($url . "/logo.jpg"));
+        @php
+            function does_url_exists($url) {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_NOBODY, true);
+        curl_exec($ch);
+        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if ($code == 200) {
+            $status = true;
+        } else {
+            $status = false;
+        }
+        curl_close($ch);
+        return $status;
+    }
+        @endphp
+        @if(does_url_exists($url.'/logo.jpg'));
         <img src="{!! $url !!}/logo.jpg" alt="">
         @else
             <h1 class="text-white font-bold text-5xl">{{ $movie->name }}</h1>

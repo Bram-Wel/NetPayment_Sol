e7e29ee74ec8dc66c5208fd1853887fc56580b0b
fabianmuema <muemafabian@gmail.com>
2021-02-10 11:32:33 +0300

    change ffmpeg

--- app/Console/Commands/AddMovies.php
+++ app/Console/Commands/AddMovies.php
@@ -124,7 +124,7 @@ class AddMovies extends Command
                         $file = escapeshellarg($file);
                         $directory = escapeshellarg($directory);
                         chdir("/run/media/thetechglitch/MOVIES/");
-                        shell_exec("ffmpeg -i $file -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 2800k -maxrate 2996k -bufsize 4200k -b:a 128k -hls_segment_filename $directory/720p_%03d.ts $directory/playlist.m3u8")
+                        shell_exec("ffmpeg -i $file -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 2800k -maxrate 2996k -bufsize 4200k -b:a 128k -hls_segment_filename $directory/720p_%03d.ts $directory/playlist.m3u8");
                         shell_exec("rm $file");
                     }
                 }

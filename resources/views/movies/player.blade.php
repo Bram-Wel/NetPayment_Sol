a8c930c1b8372712682c127405eef4ab50685259
fabianmuema <muemafabian@gmail.com>
2021-02-10 11:07:27 +0300

    remove bg

--- resources/views/movies/player.blade.php
+++ resources/views/movies/player.blade.php
@@ -12,6 +12,8 @@
         src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
+    <script defer nomodule="" src="{{ asset('js/assets/ionicons.min.js') }}"></script>
+
 
     <style>
         body {

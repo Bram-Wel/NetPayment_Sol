dc487aa0affc0781338e3cb097eae83517d4b8e9
fabianmuema <muemafabian@gmail.com>
2021-02-18 07:53:34 +0300

    remove defer

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -177,7 +177,7 @@
             <h1 class="font-bold text-xl md:pl-6 text-center md:text-left">{{ $g->genre }}</h1>
             <div id="container" class="mr-4 grab flex justify-center">
                 @php
-                    $movies = \App\Models\Genre::where('genre', $g->genre)->select('name')->inRandomOrder()->groupBy('name')->get();
+                    $movies = \App\Models\Genre::where('genre', $g->genre)->select('name')->limit(7)->inRandomOrder()->groupBy('name')->get();
                 @endphp
                 @foreach($movies as $movie)
                     <div>

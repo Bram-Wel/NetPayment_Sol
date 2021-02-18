689c3c0ac45638d631e51e9f46fbbfc9568e808c
fabianmuema <muemafabian@gmail.com>
2021-02-18 08:28:42 +0300

    siz images

--- resources/views/movies/layouts/footer.blade.php
+++ resources/views/movies/layouts/footer.blade.php
@@ -1,8 +1,11 @@
 @stack('modals')
 
-@livewireScripts
 @stack("scripts")
 <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
-<script>document.addEventListener('touchstart', onTouchStart, {passive: true});</script>
+<script>
+    if ('ontouchstart' in document.documentElement) {
+        document.addEventListener('touchstart', onTouchStart, {passive: true});
+    }
+</script>
 </body>
 </html>

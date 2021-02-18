edab80a3e64cdb84b33ea20af84b2be6110d7cbd
fabianmuema <muemafabian@gmail.com>
2021-02-18 20:02:24 +0300

    payment

--- resources/views/movies/layouts/default.blade.php
+++ resources/views/movies/layouts/default.blade.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @livewireStyles
     <script src="{{ asset('/js/assets/lazysizes.js') }}" async></script>
     <script src="{{ asset('js/assets/jquery-3.5.1.min.js') }}"></script>

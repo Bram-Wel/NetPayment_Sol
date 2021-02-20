98ee00069376f52a7795cad101333e8d6b5c414e
fabianmuema <muemafabian@gmail.com>
2021-02-20 10:13:34 +0300

    add class

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -275,11 +275,12 @@
         }
     }
 
+    const video = document.querySelector("#video");
+
     const isVideoPlaying = video => !!(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2);
 
     $('#play').bind("click keydown keyup", playTrailer);
 
-    const video = document.querySelector("#video");
     let playState = null;
 
     const observer = new IntersectionObserver((entries) => {

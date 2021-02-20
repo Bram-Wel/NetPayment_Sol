93d03564908691b35a29480cdf4384814595532b
fabianmuema <muemafabian@gmail.com>
2021-02-20 09:57:28 +0300

    add class

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -91,7 +91,7 @@
                 @endphp
                 @if(does_url_exists($url.'/logo.jpg'))
                     <div
-                        style="background: url('{!! $url !!}/logo.jpg'); width: 350px; height: 200px;
+                        style="background: url('{!! $url !!}/logo.jpg'); width: 350px; height: 130px;
                             background-size: contain; background-repeat: no-repeat; background-position: 0% 50%"
                         class="mb-4 lazyload"></div>
                 @else

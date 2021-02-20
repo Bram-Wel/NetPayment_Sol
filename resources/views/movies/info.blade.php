1c9c5b42a41d38afb165e7c0b051e27f99155a9b
fabianmuema <muemafabian@gmail.com>
2021-02-20 09:52:37 +0300

    logo to background

--- resources/views/movies/info.blade.php
+++ resources/views/movies/info.blade.php
@@ -41,7 +41,7 @@
         @if(does_url_exists($url.'/logo.jpg'))
             <div
                 style="background: url('{!! $url !!}/logo.jpg'); width: 350px; height: 100px;
-                    background-size: contain; background-position: 0% 50%"
+                    background-size: contain; background-repeat: no-repeat; background-position: 0% 50%"
                 class="mb-4 lazyload"></div>
         @else
             <h1 class="text-white font-bold text-5xl">{{ $movie->name }}</h1>

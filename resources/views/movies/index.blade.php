b4ed99fdb3287503834da4d6c894bbd2416b0204
fabianmuema <muemafabian@gmail.com>
2021-02-20 10:21:58 +0300

    add class

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -264,7 +264,7 @@
 
     const video = document.querySelector("#video");
 
-    const isVideoPlaying = video => !!(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2);
+    const isVideoPlaying = video => !!(!video.paused && !video.ended && video.readyState > 2);
 
     function playTrailer() {
         let video = $('#video');

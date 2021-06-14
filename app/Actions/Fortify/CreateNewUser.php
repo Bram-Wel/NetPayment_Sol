e52df152ec45826d33ad60f93b2f5c45c66d1223
fabianmuema <muemafabian@gmail.com>
2021-06-14 23:17:10 +0300

    TRIAL ONLY WHEN mac doesnt exist in table

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -2,15 +2,16 @@
 
 namespace App\Actions\Fortify;
 
+use Carbon\Carbon;
+use RouterOS\Query;
 use App\Models\Team;
 use App\Models\User;
-use Carbon\Carbon;
+use RouterOS\Client;
+use RouterOS\Config;
+use App\Models\Trial;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
-use RouterOS\Client;
-use RouterOS\Config;
-use RouterOS\Query;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -31,7 +32,6 @@ class CreateNewUser implements CreatesNewUsers
         ])->validate();
 
         return DB::transaction(function () use ($input) {
-
             $config = new Config([
                 'host' => env('MIKROTIK_HOST'),
                 'user' => env('MIKROTIK_USERNAME'),
@@ -46,35 +46,39 @@ class CreateNewUser implements CreatesNewUsers
                 ->equal('name', $input['username'])
                 ->equal('server', env('MIKROTIK_HOTSPOT_SERVER'))
                 ->equal('password', $password)
-                ->equal('profile', env('MIKROTIK_TRIAL_PACKAGE'));
+                ->equal('profile', Trial::where('mac', session()->get('mac'))->doesntExist() ? env('MIKROTIK_TRIAL_PACKAGE') : '0MBPS');
 
             $client->q($query)->read();
 
             $now = Carbon::now('Africa/Nairobi');
-            $end = $now->addDays(1);
 
-            $date = date('M/d/Y', strtotime($end));
-            $time = date('H:i:s', strtotime($end));
-            $username = $input['username'];
-            $source = "/ip hotspot active remove [find user=$username];
-             /ip hotspot user set profile=0MBPS [find name=$username];
-              /ip hotspot cookie remove [find user=$username];
-              /system scheduler remove [find name=deactivate-$username];";
+            // check if mac in trails table
+            if (Trial::where('mac', session()->get('mac'))->doesntExist()) {
+                $end = $now->addDays(1);
 
-            $query = (new Query('/system/scheduler/add'))
-                ->equal('name', "deactivate-$username")
-                ->equal('start-date', $date)
-                ->equal('start-time', $time)
-                ->equal('on-event', $source);
+                $date = date('M/d/Y', strtotime($end));
+                $time = date('H:i:s', strtotime($end));
+                $username = $input['username'];
+                $source = "/ip hotspot active remove [find user=$username];
+                /ip hotspot user set profile=0MBPS [find name=$username];
+                /ip hotspot cookie remove [find user=$username];
+                /system scheduler remove [find name=deactivate-$username];";
 
-            $response = $client->query($query)->read();
+                $query = (new Query('/system/scheduler/add'))
+                    ->equal('name', "deactivate-$username")
+                    ->equal('start-date', $date)
+                    ->equal('start-time', $time)
+                    ->equal('on-event', $source);
 
-            $query = (new Query('/ip/hotspot/active/login'))
-                ->equal('ip', session()->get('ip'))
-                ->equal('user', "$username")
-                ->equal('password', $input['password']);
+                $response = $client->query($query)->read();
 
-            $response = $client->q($query)->read();
+                $query = (new Query('/ip/hotspot/active/login'))
+                    ->equal('ip', session()->get('ip'))
+                    ->equal('user', "$username")
+                    ->equal('password', $input['password']);
+
+                $response = $client->q($query)->read();
+            }
 
             return tap(User::create([
                 'username' => $input['username'],

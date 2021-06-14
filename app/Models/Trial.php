63181d5d1133fd7b04963d75fadc0907b9741764
fabianmuema <muemafabian@gmail.com>
2021-06-14 23:08:22 +0300

    trials table

--- app/Models/Trial.php
+++ app/Models/Trial.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Volume extends Model
+class Trial extends Model
 {
     use HasFactory;
 }
---
+++ database/migrations/2021_06_14_230650_create_trials_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trials', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('address');
+            $table->string('mac');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trials');
+    }
+}
